0fbf12234be6f89246b0a4bb2b673a871eeb105f
zhangshuaishuai <shuaishuaizhang@taichi.graphics>
2024-02-26 14:26:30 +0800

    test

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import {run} from './handle'
 async functon run(): Promise<void> {
     try {
         core.info('hello world')
+        const username = core.getInput("user_name")
+        core.info("hello ${user_name}")
+        core.info('user_name === admin : ${user_name === "admin"}')
     } catch (error) {
      core.setFailed(error.message)
     }
